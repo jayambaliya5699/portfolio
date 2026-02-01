0bdbc0f5e6b70d871a41aadfc078df21d791c389
thetadirect <jay.ambaliya@thetadirect.com>
2026-02-01 12:20:03 +0530

    New portfolio

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+window.onload = () => {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+        document.body.classList.add("dark");
+    }
+};
